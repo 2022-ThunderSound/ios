2dad415066816f0f2ca9f3bb2aab07fd45c463a5
juanjof20 <74968048+juanjof20@users.noreply.github.com>
2022-06-10 13:03:42 +0200

    comentarios SearchPostController

--- ThunderSound/ThunderSound/SearchPostController.swift
+++ ThunderSound/ThunderSound/SearchPostController.swift
@@ -10,15 +10,18 @@ import WebKit
 
 class SearchPostController: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
+    //  Variables
+    var songs: [[String: Any]] = []                                                                         //  Guardamos las canciones
+    var mySearch: [String: Any] = [:]                                                                       //  Guardamos los datos de la peticion
     @IBOutlet var tableView: UITableView!
     @IBOutlet var searchTF: UITextField!
     @IBOutlet var searchBT: UIButton!
     @IBAction func searchBT(_ sender: Any)
     {
         if searchTF.text != nil
-        {
-            peticionSearchSong(searchTF: searchTF.text!)
-        } else
+        {                                                                                                   //  Comprobamos que el buscador no esta vacio
+            peticionSearchSong(searchTF: searchTF.text!)                                                    //  para enviarlo por la peticion, si esta vacio
+        } else                                                                                              //  salta un Alert de error
         {
             let alert = UIAlertController(title: "Error", message: "Por favor, rellene el campo para realizar la búsqueda.", preferredStyle: .alert)
             let action = UIAlertAction(title: "Entendido", style: .default, handler: nil)
@@ -32,30 +35,30 @@ class SearchPostController: UIViewController, UITableViewDelegate, UITableViewDa
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        
-        let path = UIBezierPath(roundedRect:searchBT.bounds,
+     
+        let path = UIBezierPath(roundedRect:searchBT.bounds,                                        //  Con UIBezierPath estamos redondeando solo las esquinas que indicamos
                                 byRoundingCorners:[.topRight, .bottomRight],
                                 cornerRadii: CGSize(width: 6, height:  6))
         let maskLayer = CAShapeLayer()
         maskLayer.path = path.cgPath
         searchBT.layer.mask = maskLayer
-        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))        //  Para que al pulsar fuera del teclado se cierre
         tap.cancelsTouchesInView = false
         view.addGestureRecognizer(tap)
         
     }
-    var songs: [[String: Any]] = []
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         songs.count
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat        //  Colocamos la altura de la celda
     {
         return 200
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell      //  Colocamos los datos en las variables y guardamos el id_track
     {
         let cell = tableView.dequeueReusableCell(withIdentifier: "searchPostCell", for: indexPath) as! SearchPostTableViewCell
         let id_track = self.songs[indexPath.row]["id"]!
@@ -66,7 +69,7 @@ class SearchPostController: UIViewController, UITableViewDelegate, UITableViewDa
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)                   //  Al selecciona nos lleva a la pantalla de crear post
     {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "CreatePostid") as! AlertController
@@ -74,8 +77,7 @@ class SearchPostController: UIViewController, UITableViewDelegate, UITableViewDa
         self.present(vc, animated: true, completion: nil)
     }
     
-    var mySearch: [String: Any] = [:]
-    func peticionSearchSong(searchTF: String)
+    func peticionSearchSong(searchTF: String)                                                       //  Peticion encontrar cancion mediante GET y a la API de Spotify
     {
         let Url = String(format: "https://v1.nocodeapi.com/thundersound/spotify/KXtTVPLnmLOAqHgA/search?q=\(searchTF)&type=track&perPage=6")
         guard let serviceUrl = URL(string: Url) else { return }
@@ -97,7 +99,7 @@ class SearchPostController: UIViewController, UITableViewDelegate, UITableViewDa
                         self.songs = dataG["items"] as! [[String : Any]]
                         DispatchQueue.main.async
                         {
-                            self.tableView.reloadData()
+                            self.tableView.reloadData()                                             //  Si todo va bien se recarga el tableView con las canciones
                         }
                     } else
                     {
