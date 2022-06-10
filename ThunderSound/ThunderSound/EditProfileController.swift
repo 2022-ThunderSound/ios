a419cdfc1a64507fbf29a80168e2bc0b33aaf0cc
juanjof20 <74968048+juanjof20@users.noreply.github.com>
2022-06-10 13:23:32 +0200

    comentarios EditProfileController

--- ThunderSound/ThunderSound/EditProfileController.swift
+++ ThunderSound/ThunderSound/EditProfileController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class EditProfileController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate
 {
+    //  Variables
+    var myDictionary: [String: Any] = [:]                                                           //  Almacenamos el data de la peticion
     @IBAction func atrasBT(_ sender: Any) {dismiss(animated: true, completion: nil)}
     @IBOutlet var userTF: UITextField!
     @IBOutlet var passTF: UITextField!
@@ -16,11 +18,11 @@ class EditProfileController: UIViewController, UITextFieldDelegate, UIImagePicke
     @IBOutlet var descripcionTF: UITextField!
     @IBOutlet var editarIMG: UIImageView!
     @IBAction func addIMG(_ sender: Any)
-    {
+    {                                                                                               //  Boton para elegir foto de perfil
         let ac = UIAlertController(title: "Seleccionar Imagen", message: "Seleccione una imagen", preferredStyle: .actionSheet)
-        let cameraBT = UIAlertAction(title: "Camara", style: .default)
-        { (_) in
-            self.showImagePicker(selectedSource: .camera)
+        let cameraBT = UIAlertAction(title: "Camara", style: .default)                              //  Creamos un Alert para decidir de donde
+        { (_) in                                                                                    //  sacaremos nuestra imagen de perfil y
+            self.showImagePicker(selectedSource: .camera)                                           //  llamamos a la funcion correspondiente
         }
         let galeriaBT = UIAlertAction(title: "Galeria", style: .default)
         { (_) in
@@ -32,7 +34,7 @@ class EditProfileController: UIViewController, UITextFieldDelegate, UIImagePicke
         ac.addAction(cancelBT)
         self.present(ac, animated: true, completion: nil)
     }
-    @IBAction func guardarBT(_ sender: Any)
+    @IBAction func guardarBT(_ sender: Any)                                                          //  Boton para enviar peticion de Editar Perfil
     {
         var bodyData = ""
         let shared = UserDefaults.standard
@@ -42,13 +44,12 @@ class EditProfileController: UIViewController, UITextFieldDelegate, UIImagePicke
             
             if userTF.text != nil
             {
-                bodyData = "nick=\(userTF.text!)"
+                bodyData = "nick=\(userTF.text!)" // hay que probar a pasarlos por json en vez de body
             }
             print(bodyData)
             
-            let id = shared.integer(forKey: "id")
-                
-                
+            
+            let id = shared.integer(forKey: "id")       
             peticionEditarPerfil(id: id, bodyData: bodyData)
         } else
         {
@@ -61,12 +62,12 @@ class EditProfileController: UIViewController, UITextFieldDelegate, UIImagePicke
     
     override func viewDidLoad()
     {
-        super.viewDidLoad()
-        self.editarIMG.layer.cornerRadius = 55
+        super.viewDidLoad()                                                                             //  Redondeamos la imagen de perfil y 
+        self.editarIMG.layer.cornerRadius = 55                                                          //  el cuadro de descripcion
         self.editarIMG.clipsToBounds = true
         self.descripcionTF.layer.cornerRadius = 10
         self.descripcionTF.clipsToBounds = true
-        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))            //  Desactivar teclado al pulsar fuera
         tap.cancelsTouchesInView = false
         view.addGestureRecognizer(tap)
     }
@@ -76,15 +77,14 @@ class EditProfileController: UIViewController, UITextFieldDelegate, UIImagePicke
         
     }
     
-    var myDictionary: [String: Any] = [:]
-    func peticionEditarPerfil(id: Int, bodyData: String)
+    func peticionEditarPerfil(id: Int, bodyData: String)//Aqui vendria el jsonData
     {
         let imgString = editarIMG.image?.pngData()?.base64EncodedString()
         let Url = String(format: "http://35.181.160.138/proyectos/thunder22/public/api/usuarios/\(id)")
         guard let serviceUrl = URL(string: Url) else { return }
         var request = URLRequest(url: serviceUrl)
         request.addValue("Application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        request.httpMethod = "PUT/PATCH" //EDITAR
+        request.httpMethod = "PUT" //EDITAR
 
         
 //        let bodyData = "?password=\(passTF.text!)&nick=\(userTF.text!)&descripcion=\(descripcionTF.text!)&foto_url=\(String(describing: imgString))"
@@ -104,16 +104,14 @@ class EditProfileController: UIViewController, UITextFieldDelegate, UIImagePicke
                         self.myDictionary = json as! [String: Any]
                         print(json)
                         
-                        if self.myDictionary["error"] as? String == nil
+                        if self.myDictionary["error"] as? String == nil                                                 //  Si no recibimos ningun error...
                         {
-                            
                             let storyboard = UIStoryboard(name: "Main", bundle: nil)
                             let vc = storyboard.instantiateViewController(withIdentifier: "Profileid") as! PerfilController
-                            vc.modalPresentationStyle = .fullScreen
+                            vc.modalPresentationStyle = .fullScreen                                                     //  Nos lleva a nuestro perfil 
                             self.present(vc, animated: true, completion: nil)
-                            
                         } else
-                        {
+                        {                                                                                               //  Sino salta un error con los posibles errores
                             let alert = UIAlertController(title: "Error != 200", message: self.myDictionary["message"] as? String, preferredStyle: .alert)
                             let action = UIAlertAction(title: "Entendido", style: .default, handler: nil)
                             alert.addAction(action)
@@ -128,9 +126,9 @@ class EditProfileController: UIViewController, UITextFieldDelegate, UIImagePicke
         }.resume()
     }
 
-    func showImagePicker(selectedSource: UIImagePickerController.SourceType)
-    {
-        guard UIImagePickerController.isSourceTypeAvailable(selectedSource) else
+    func showImagePicker(selectedSource: UIImagePickerController.SourceType)                                    //  Estas tres funciones estan permitiendo
+    {                                                                                                           //  que podamos coger una imagen de nuestra
+        guard UIImagePickerController.isSourceTypeAvailable(selectedSource) else                                //  galeria o camara del telefono
         {
             print("Recurso seleccionado no disponible.")
             return
